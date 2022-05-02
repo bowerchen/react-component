3ad8889ead69d437612961fdfa9cb312bdaa3f30
bowerchen <bowerfullstack@gmail.com>
2022-05-02 22:15:50 +0800

    完成button基本组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Button, {ButtonType, ButtonSize} from './components/Button/button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello world</Button>
+        <Button disabled btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello world</Button>
+        <Button btnType={ButtonType.Link} href="https://www.baidu.com">baidu</Button>
+        <Button disabled btnType={ButtonType.Link} href="https://www.baidu.com">baidu</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
